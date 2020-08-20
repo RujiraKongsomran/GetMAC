3829371988953efbee5862edb7129f9ace2ba5d1
RujiraKongsomran <rujirabiwberry@gmail.com>
2020-08-20 08:00:14 +0700

    Get MAC Address finish

--- app/src/main/java/com/rujirakongsomran/getmac/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/getmac/MainActivity.kt
@@ -3,6 +3,8 @@ package com.rujirakongsomran.getmac
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rujirakongsomran.getmac.databinding.ActivityMainBinding
+import java.net.NetworkInterface
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,31 @@ class MainActivity : AppCompatActivity() {
         //setContentView(R.layout.activity_main)
 
         setContentView(binding.root)
-        binding.tvMAC.text = "ZZZZ"
+        var macNumber = getMacAddress()
+        binding.tvMAC.text = macNumber
+    }
+
+    private fun getMacAddress(): String? {
+        try {
+            val networkInterfaceList: List<NetworkInterface> =
+                Collections.list(NetworkInterface.getNetworkInterfaces())
+            for (networkInterface in networkInterfaceList) {
+                if (networkInterface.name != "wlan0") {
+                    continue
+                }
+                val macAddress: ByteArray = networkInterface.hardwareAddress ?: return ""
+                val result = StringBuilder()
+                for (data in macAddress) {
+                    result.append(Integer.toHexString(data.toInt() and 0xFF)).append(":")
+                }
+                if (result.isNotEmpty()) {
+                    result.deleteCharAt(result.length - 1)
+                }
+                return result.toString()
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+        return "02:00:00:00:00:00"
     }
 }
